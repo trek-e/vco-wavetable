cda6af23fc4214c42fbb3020133175e01f1e639b
Tom Boucher <trekkie@nomorestars.com>
2026-03-13 23:28:44 -0400

    chore: add GPLv3 license file and GPL headers to plugin.cpp/hpp

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,3 +1,13 @@
+/*
+ * Hurricane 8 — Plugin Registration
+ * Copyright (C) 2026 Synth-etic Intelligence
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 #include "plugin.hpp"
 
 using namespace rack;
